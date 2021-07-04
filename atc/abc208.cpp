e394fdb35321f88b3bb17fc791a38bba375d344a
congyu711 <sxlxcsxlxc@gmail.com>
2021-07-04 23:45:10 +0800

    upd solution for w and a useless source file

--- atc/abc208.cpp
+++ atc/abc208.cpp
@@ -5,7 +5,10 @@ const int N = 1e6+10;
 
 void solve()
 {
-    
+    int a,b;
+    cin>>a>>b;
+    if((double)b/(double)a>6.0||(double)b/(double)a<1.0) cout<<"No"<<endl;
+    else cout<<"Yes"<<endl;
 }
 int main()
 {
