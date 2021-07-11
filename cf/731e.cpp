a451f65dafd5de2b8286ed1a75e754695d48d3cc
congyu711 <sxlxcsxlxc@gmail.com>
2021-07-11 08:53:44 +0800

    cf731div3

---
+++ cf/731e.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 3e5 + 10;
+typedef long long LL;
+const LL inf = INTMAX_MAX;
+const int mod = 1e9+7;
+int n,k;
+LL a[N],t[N];
+void solve()
+{
+    cin>>n>>k;
+    for(int i=1;i<=k;i++)   cin>>a[i];
+    for(int i=1;i<=k;i++)   cin>>t[i];
+    
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--)
+        solve();
+}
+
+/*
+1
+4
+1 2 4 8
+*/
