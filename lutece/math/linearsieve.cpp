5d72ebf4fde583d7a8da875d7a654437538ed34a
congyu711 <sxlxcsxlxc@gmail.com>
2021-09-08 10:56:22 +0800

    upd linearsieve

--- lutece/math/linearsieve.cpp
+++ lutece/math/linearsieve.cpp
@@ -3,20 +3,19 @@ using namespace std;
 typedef long long LL;
 const int N=5e5+10;
 const LL mod=1e9+7;
-// #ifdef CONGYU
-//     ifstream fin("testcase.in");
-// #endif
-//a(n) = Sum_{k=1..n} mu(n/gcd(n,k))*sigma(gcd(n,k)).
 bool vis[N];
-int prime[N],o[N],m[N],a1[N],a0[N],f[N];
+int prime[N],f[N];
 int n,tot;
 int minp[N],ppow[N];//最小质因子次数，能约掉最小质因子的k次方
+
+// 欧拉函数为例 f[N]
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin>>n;
-    vis[1]=1;o[1]=1;m[1]=1;a1[1]=1;a0[1]=1;f[1]=1;
+    auto F=[&](LL p,LL k,LL p_k)->LL{return p_k-p_k/p;};
+    vis[1]=1;f[1]=1;
     for(int i=2;i<=n;i++)
     {
         if(!vis[i])
@@ -24,7 +23,7 @@ int main()
             prime[++tot]=i;
             minp[i]=1;
             ppow[i]=i;
-            o[i]=i-1;a0[i]=2;a1[i]=i+1;m[i]=-1;
+            f[i]=F(i,1,i);
         }
         for(int j=1;j<=tot;j++)
         {
@@ -33,13 +32,14 @@ int main()
             {
                 minp[i*prime[j]]=minp[i]+1;
                 ppow[i*prime[j]]=ppow[i]*prime[j];
-
+                f[i*prime[j]]=F(prime[j],minp[i*prime[j]],ppow[i*prime[j]])*f[i/ppow[i]];
                 break;
             }
             //这里的数都是被一个最小质因子筛掉了
             minp[i*prime[j]]=1;
             ppow[i*prime[j]]=prime[j];
-
+            f[i*prime[j]]=f[i]*F(prime[j],1,prime[j]);
         }
     }
+    for(int i=1;i<=n;i++)   cout<<f[i]<<endl;
 }
