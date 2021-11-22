314af5687cfbb012221fe1e74d1c569f35b06a21
congyu711 <sxlxcsxlxc@gmail.com>
2021-11-22 15:27:26 +0800

    upd karatsuba

--- dsct/karatsuba.cpp
+++ dsct/karatsuba.cpp
@@ -40,6 +40,26 @@ bigint add(bigint a,bigint b)
     if(carry)   res.push_back(carry);
     return res;
 }
+bigint subtract(bigint a,bigint b)
+{
+    bigint tmp,res;
+    for(int i=0;i<b.size();i++)  tmp.push_back(b[i]);
+    for(int i=0;i<a.size()-b.size();i++)    tmp.push_back(0);
+    int n=a.size();
+    int bit=0;
+    for(int i=0;i<n;i++)
+    {
+        a[i]-=bit;
+        if(a[i]<tmp[i])
+        {
+            bit=1;
+            a[i]+=10;
+        }
+        else bit=0;
+        res.push_back(a[i]-tmp[i]);
+    }
+    return res;
+}
 bigint leftshift(bigint c,int n)
 {
     bigint res;
@@ -52,7 +72,7 @@ bigint karatsuba(bigint a,bigint b)
 {
     while(a.size()<b.size())    a.push_back(0);
     while(b.size()<a.size())    b.push_back(0);
-    bigint res,la,ra,lb,rb;
+    bigint res=ulltobigint(0),la,ra,lb,rb;
     if(a.size()==0) return ulltobigint(0);
     if(a.size()==1) return ulltobigint(a[0]*b[0]);
     int m=a.size()/2,sz=a.size();
@@ -67,14 +87,41 @@ bigint karatsuba(bigint a,bigint b)
         lb.push_back(b[i]);
     }
 
+    // normal divide & conquer
+    // res=leftshift(karatsuba(la,lb),m*2);
+    // res=add(res,leftshift(add(karatsuba(la,rb),karatsuba(ra,lb)),m));
+    // res=add(res,karatsuba(ra,rb));
 
-    res=leftshift(karatsuba(la,lb),m*2);
-    res=add(res,leftshift(add(karatsuba(la,rb),karatsuba(ra,lb)),m));
-    res=add(res,karatsuba(ra,rb));
+    // karatsuba
+    bigint c=add(la,ra),d=add(lb,rb);
+    bigint s=karatsuba(c,d);
+    bigint f=karatsuba(la,lb);
+    bigint t=karatsuba(ra,rb);
+    s=subtract(s,f);
+    s=subtract(s,t);
+    res=add(res,leftshift(f,m*2));
+    res=add(res,leftshift(s,m));
+    res=add(res,t);
     return res;
 }
 int main()
 {
-    print(karatsuba(ulltobigint(11),ulltobigint(111)));
-    cout<<endl<<111*11<<endl;
+    print(karatsuba(ulltobigint(1213),ulltobigint(112)));
+    cout<<endl<<1213*112<<endl;
+    // mt19937 gen(time(nullptr));
+    // while(1)
+    // {
+    //     ULL a=gen()%2333,b=gen()%2333;
+    //     ULL c=max(a,b),d=min(a,b);
+    //     bigint e,f;
+    //     e=ulltobigint(c-d);
+    //     f=subtract(ulltobigint(c),ulltobigint(d));
+    //     if(e==f)    continue;
+    //     else
+    //     {
+    //         cout<<c<<' '<<d<<endl;
+    //         print(e);cout<<endl;print(f);cout<<endl;
+    //         break;
+    //     }
+    // }
 }
